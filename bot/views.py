39b431837472aaf5c19dcee21057b33ed8c4298b
ekhoshnood <e.khoshnood966@gmail.com>
2019-12-08 16:18:45 +0330

    button 1

--- bot/views.py
+++ bot/views.py
@@ -9,6 +9,7 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 bot = telebot.TeleBot('994604539:AAGsEMWEDW93la7rQ5c0JYv62sHiVTu5X1g')
 
+
 class UpdateBot(APIView):
     def post(self, request):
         json_string = request.body.decode("UTF-8")
@@ -37,55 +38,6 @@ class ButtonText(APIView):
         except:
             return Response({"code": 401})
 
-'''
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, 'سلام {}'.format(message.from_user.first_name))
-    user = User()
-    user.user_id = message.chat.id
-    user.save()
-
-@bot.message_handler(commands=['هی'])
-def hei(message):
-    bot.send_message(message.chat.id, 'با هر ردیف کلمه /بعدی را تایپ کنید')
-    hei = 0
-    if hei==0:
-        bot.send_message(message.chat.id, 'شر شر بارون، توی خیابون \n')
-    if hei==1:
-        bot.send_message(message.chat.id, 'دختره گولم زد، دست به دولم زد \n')
-    if hei==2:
-        bot.send_message(message.chat.id, 'دولم بزرگ شد، کردم تو کونش \n')
-    if hei==3:
-        bot.send_message(message.chat.id, 'کونش خون اومد، بردم به دکتر \n')
-    if hei==4:
-        bot.send_message(message.chat.id, 'دکتر دواش داد، آب انار داد \n')
-    if hei==5:
-        bot.send_message(message.chat.id, 'امروز خورد، فردا مرد \n')
-    if hei==6:
-        bot.send_message(message.chat.id, 'مرد دیگه، میخاستی چی بشه؟ \n')
-    hei+=1
-
-@bot.message_handler(content_types='text')
-def send_Message(message):
-    bot.send_message(message.chat.id, 'روی هی/ کلیک کنید')
-    if message==0:
-        bot.send_message(message.chat.id, 'شر شر بارون، توی خیابون \n')
-        bot.send_message(message.chat.id, '/1')
-    if message==1:
-        bot.send_message(message.chat.id, 'دختره گولم زد، دست به دولم زد \n')
-    if message==2:
-        bot.send_message(message.chat.id, 'دولم بزرگ شد، کردم تو کونش \n')
-    if message==3:
-        bot.send_message(message.chat.id, 'کونش خون اومد، بردم به دکتر \n')
-    if message==4:
-        bot.send_message(message.chat.id, 'دکتر دواش داد، آب انار داد \n')
-    if message==5:
-        bot.send_message(message.chat.id, 'امروز خورد، فردا مرد \n')
-    if message==6:
-        bot.send_message(message.chat.id, 'مرد دیگه، میخاستی چی بشه؟ \n')
-
-'''
-
 
 def Button(message):
     r = requests.get('https://wikishop.herokuapp.com/api/button')
@@ -99,6 +51,7 @@ def Button(message):
     bot.send_message(message.from_user.id, text, reply_markup=key)
 
 
+'''
 @bot.message_handler(commands=['start'])
 def start(message):
     Button(message)
@@ -116,4 +69,29 @@ def send_message(message):
         wiki = data['text']
         bot.send_message(message.from_user.id, wiki)
 
-bot.polling()
+# bot.polling()
+
+
+'''
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'سلام {}'.format(message.from_user.first_name))
+    user = User()
+    user.user_id = message.chat.id
+    user.save()
+    data = {'list': []}
+    button = Button.objects.all()
+    for i in button:
+        data['list'].append({'name': i.button})
+
+
+@bot.message_handler(commands=['هی'])
+def hei(message):
+    bot.send_message(message.chat.id, 'با هر ردیف کلمه /بعدی را تایپ کنید')
+
+
+@bot.message_handler(content_types='text')
+def send_Message(message):
+    bot.send_message(message.chat.id, 'روی هی/ کلیک کنید')
